97b6ce3a71934cca367c32f0efdb43de1059090e
naterrie <nathanterrier38580@gmail.com>
2024-12-10 14:46:25 +0100

    adding comments and update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # libasm
 Project libasm of 42
 Consisting of doing a library in ASM in 64 bits with Intel syntax
+Compilation : make for a classic compilation with libasm.a and run who compile the .a and the main then run the program
 
-nasm -f elf64 src/
--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -4,13 +4,13 @@ section .text
 	global ft_strlen
 
 	ft_strlen:
-		mov rax, 0
+		mov rax, 0 ; counter
 
 	_while:
-		cmp BYTE [rdi + rax], 0x0
-		je _end
-		inc rax
-		jmp _while
+		cmp BYTE [rdi + rax], 0x0 ; check if the current character is the null terminator
+		je _end ; if it is, return
+		inc rax ; increment the counter
+		jmp _while ; loop
 
 	_end:
 		ret
