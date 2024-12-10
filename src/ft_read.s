130fcbd7d9ba45bd3761864336cbeeb9b5d94bc8
naterrie <nathanterrier38580@gmail.com>
2024-12-10 15:54:26 +0100

    adding read and write

---
+++ src/ft_read.s
@@ -0,0 +1,22 @@
+bits 64
+extern __errno_location
+
+section .text
+	global ft_read
+
+	ft_read:
+		mov rax, 0 ; syscall number for read
+		syscall ; call read
+		cmp rax, 0 ; check if the return value is 0
+		jl _error ; if it is, return
+		ret
+
+	_error:
+		neg rax ; errno to positive value
+		mov r8, rax ; save errno
+		call __errno_location wrt ..plt; get errno location in rax
+		mov [rax], r8 ; save errno in the location
+		mov rax, -1
+		ret
+
+
---
+++ src/ft_write.s
@@ -0,0 +1,22 @@
+bits 64
+extern __errno_location
+
+section .text
+	global ft_write
+
+	ft_write:
+		mov rax, 1 ; syscall number for write
+		syscall ; call write
+		cmp rax, 0 ; check if the return value is 0
+		jl _error ; if it is, return
+		ret
+
+	_error:
+		neg rax ; errno to positive value
+		mov r8, rax ; save errno
+		call __errno_location wrt ..plt; get errno location in rax with linker
+		mov [rax], r8 ; save errno in the location pointed by rax
+		mov rax, -1
+		ret
+
+
